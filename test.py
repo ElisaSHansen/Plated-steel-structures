383bdb35419924d03da2a2045331d4dd0e064b8e
mariannehou <marianne.hou@gmail.com>
2026-02-09 11:21:43 +0100

    hello

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-#hei
-#hei
+
+a=2800 #mm
+b=670 #mm
+v=0.3
+E=210000
+def defelction_rigity(h):
+    D=(E*h**3)/(12*(1-v**2))
+    return D
+print(defelction_rigity(6.5))
+
+def deflection(m,n):
+    
